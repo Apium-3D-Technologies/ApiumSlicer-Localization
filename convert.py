e0cb606fb7c924713708d85708e8a591b48699aa
Julian Bruyers <julian.bruyers@apiumtec.com>
2024-10-19 11:27:31 +0000

    chore: Update bundled resources

---
+++ convert.py
@@ -0,0 +1,23 @@
+from polib import pofile
+
+
+# Takes an .po file as an input and makes a copy of given file, where
+# the translation for each text is equal to the original text. The tool
+# was used to create an english translation the chenge "PrusaSlicer" to
+# "ApiumSlicer" via the Poedit software.
+
+# Path to your source .po file
+file_path = 'target.po'
+
+# Ouput file name
+output = 'result.po'
+
+po = pofile(file_path)
+
+for entry in po:
+    entry.msgstr = entry.msgid  # Set msgid as msgstr
+
+po.save(output)
+
+print("Done...")
+
