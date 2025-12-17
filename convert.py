1c9fbb44a32d90c9d25ce7742c246671c1a995a9
Julian Bruyers <julian.bruyers@apiumtec.com>
2025-12-17 03:37:46 +0100

    fix: Spelling & strings

--- convert.py
+++ convert.py
@@ -1,16 +1,15 @@
 from polib import pofile
 
-
 # Takes an .po file as an input and makes a copy of given file, where
 # the translation for each text is equal to the original text. The tool
-# was used to create an english translation the chenge "PrusaSlicer" to
+# was used to create an english translation the change "PrusaSlicer" to
 # "ApiumSlicer" via the Poedit software.
 
 # Path to your source .po file
-file_path = 'target.po'
+file_path = "target.po"
 
 # Ouput file name
-output = 'result.po'
+output = "result.po"
 
 po = pofile(file_path)
 
@@ -20,4 +19,3 @@ for entry in po:
 po.save(output)
 
 print("Done...")
-
